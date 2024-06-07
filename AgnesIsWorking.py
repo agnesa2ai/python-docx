cf9b2aa895c393fd4143af83d261c5ecda0d5249
Agnes Kim <agnes@a2-ai.com>
2024-06-07 15:50:10 +0000

    working ver

---
+++ AgnesIsWorking.py
@@ -0,0 +1,43 @@
+# clone git repo
+# https://github.com/python-openxml/python-docx?tab=readme-ov-file
+
+#pip install python-docx
+import os
+import re
+import docx
+from pathlib import Path
+from docx import Document
+from docx.shared import Inches
+from docx.enum.text import WD_BREAK
+
+
+#bring in shell 
+document = Document('pre.docx')
+
+#establish a pattern to magic string ..///
+start_pattern = r'\.\.///'
+end_pattern = r'\.png'
+magic_pattern = re.compile(start_pattern + '.*?' + end_pattern)
+
+
+#locate magic string, insert image, insert source path
+document.paragraphs  # to extract paragraphs
+for par in document.paragraphs: 
+    matches = re.findall(start_pattern, par.text)
+    print (matches)
+    if matches:
+        for match in matches:
+            match_index = par.text.find(match) 
+            figure_name = par.text[match_index + len(match):]
+            figure_location_and_name = ("images/" + figure_name)  #can change "images" later. but don't know if necessary
+            magic_matches = magic_pattern.findall(par.text) #pattern search
+            for match in magic_matches:
+                par.text = par.text.replace(match, "")
+            r = par.add_run('')  
+            r.add_picture(figure_location_and_name, width=Inches(2)) #inserting image 
+            rr = par.add_run('\n')
+            rr.add_text('Source: ' + figure_location_and_name) #inserting source path as text
+
+            
+#render out draft
+document.save('post.docx')
